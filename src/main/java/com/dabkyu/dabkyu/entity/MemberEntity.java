2a5d49169677d85afb4a20259b5a4df7d1d7efbe
nayoung99 <coco99827@naver.com>
2024-12-02 15:18:19 +0900

    24.12.02_memberEntityMEMBER_SEQ삭제

--- src/main/java/com/dabkyu/dabkyu/entity/MemberEntity.java
+++ src/main/java/com/dabkyu/dabkyu/entity/MemberEntity.java
@@ -6,10 +6,7 @@ import com.dabkyu.dabkyu.dto.MemberDTO;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,9 +24,6 @@ import lombok.Setter;
 public class MemberEntity {
 
     @Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ")	
-	@SequenceGenerator(name="MEMBER_SEQ", sequenceName = "member_seq", 
-		initialValue = 1, allocationSize = 1)
 	@Column(name="email",length=50,nullable=false)
 	private String email;
 
